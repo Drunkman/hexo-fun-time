527b0e4e8ff86b42d3c2bbf6820eaeaa0052811b
ma.gaoyu <ma.gaoyu@linecorp.com>
2019-08-27 17:30:58 +0800

    add disable and change style function

--- README.md
+++ README.md
@@ -11,8 +11,33 @@ To install, run the following command in the root directory of hexo:
 npm install hexo-fun-time --save
 ```
 
-And add this plugin in your ``_config.yml``.
-
 ## Usage
+#### simple Usage
 
 	{% funTime %}
+
+#### disable render
+If you want to disable this function, you can setting in `_config.yml`
+```yaml
+fun_time:
+  disable: true
+```
+#### change default style
+If you want to change default style, you can setting style in `_config.yml`, 
+you can set three part: body, title, content
+
+For example:
+```yaml
+fun_time:
+  style: 
+    body:
+      margin: "10px 10px 10px auto" # body align right side
+    title:
+      font-size: 50px # change font size
+    content:
+      font-size: 30px # change font size
+      
+```
+
+## Credits
+Thanks the [RollToolsApi](https://github.com/MZCretin/RollToolsApi) project for default joke resource!
--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-var axios = require('axios');
-var deasync = require('deasync');
+var getJoke = require('./lib/lib');
 
-var jokes = [];
-
-function getJokes() {
-  if (jokes.length === 0) {
-    axios.get("https://www.mxnzp.com/api/jokes/list/random").then(function (response) {
-      jokes = response.data.data;
-    });
-    deasync.loopWhile(function () {
-      return jokes.length === 0
-    });
+var defaultStyle = {
+  body: {
+    width: "400px",
+    padding: "10px",
+    margin: "10px auto",
+    border: "#000 2px dotted; border-radius: 10px"
+  },
+  title: {
+    "font-size": "24px",
+    "font-style": "italic",
+    "text-shadow": "0 0 0.2em #8F7, 0 0 0.2em #8F7"
+  },
+  content: {}
+};
+var hexoConfig = hexo.config.fun_time || {style: {body: {}, title: {}, content: {}}};
+var config = {
+  disable: hexoConfig.disable || false,
+  dynamic: hexoConfig.dynamic || false,
+  contentApi: hexoConfig.content_api || "https://www.mxnzp.com/api/jokes/list/random",
+  contentField: hexoConfig.content_field || "data",
+  style: {
+    body: Object.assign({}, defaultStyle.body, hexoConfig.style.body),
+    title: Object.assign({}, defaultStyle.title, hexoConfig.style.title),
+    content: Object.assign({}, defaultStyle.content, hexoConfig.style.content),
   }
-  return jokes.pop().content;
-}
-
-function embedHtml(content) {
-  return "<form><fieldset style='width: 400px; padding: 10px; margin: 10px auto; border: #000 2px dotted; border-radius: 10px;'>" +
-         "<legend style='font-size: 24px; text-shadow: 0 0 0.2em #8F7, 0 0 0.2em #8F7;'><i>开心一刻</i></legend><div>" +
-         content +
-         "</div></fieldset></form>";
-}
+};
 
 hexo.extend.tag.register('funTime', function (args) {
+  if (config.disable) {
+    return "";
+  }
   if (args.length !== 0) {
     if (args[0] === 'dynamic') {
       // TODO
-      return "TODO";
     }
-  } else {
-    return embedHtml(getJokes());
   }
+  return getJoke(config);
 });
